55c35f9230e3eca7ba605f22c77f1286ed8ecd2b
Allen Lancelot <alan910810@gmail.com>
2016-08-16 16:17:21 +0800

    Added ThreadLocal Example

---
+++ src/multithreading/ThreadLocalImpl.java
@@ -0,0 +1,60 @@
+package multithreading;
+
+import java.util.Random;
+
+public class ThreadLocalImpl implements Runnable {
+	private final static ThreadLocal<People> threadLocal = new ThreadLocal<People>();
+
+	public static void main(String[] agrs) {
+		ThreadLocalImpl td = new ThreadLocalImpl();
+		Thread t1 = new Thread(td);
+		Thread t2 = new Thread(td);
+		t1.start();
+		t2.start();
+	}
+
+	public void run() {
+		accessStudent();
+	}
+
+	public void accessStudent() {
+
+		String currentThreadName = Thread.currentThread().getName();
+		System.out.println(currentThreadName + " is running!");
+
+		Random random = new Random();
+		int age = random.nextInt(100);
+		System.out.println("thread " + currentThreadName + " set age to:" + age);
+		People people = getPeople();
+		people.setAge(age);
+		System.out.println("thread " + currentThreadName + " first read age is:" + people.getAge());
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("thread " + currentThreadName + " second read age is:" + people.getAge());
+	}
+
+	protected People getPeople() {
+		People people = threadLocal.get();
+
+		if (people == null) {
+			people = new People();
+			threadLocal.set(people);
+		}
+		return people;
+	}
+}
+
+class People {
+	private int age = 0;
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
