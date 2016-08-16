581875037015a3e6e28764187830573c622b3d39
Allen Lancelot <alan910810@gmail.com>
2016-08-16 17:55:56 +0800

    Fixed Formatting

--- src/com/jobjectpool/pool/BoundedBlockingPool.java
+++ src/com/jobjectpool/pool/BoundedBlockingPool.java
@@ -8,118 +8,117 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public final class BoundedBlockingPool<T> extends AbstractBlockingPool<T> {
-    private int size;
-    private BlockingQueue<T> objects;
-    private Validator<T> validator;
-    private ObjectFactory<T> objectFactory;
-    private ExecutorService executor = Executors.newCachedThreadPool();
+	private int size;
+	private BlockingQueue<T> objects;
+	private Validator<T> validator;
+	private ObjectFactory<T> objectFactory;
+	private ExecutorService executor = Executors.newCachedThreadPool();
 
-    private volatile boolean shutdownCalled;
+	private volatile boolean shutdownCalled;
 
-    public BoundedBlockingPool(int size, Validator<T> validator,
-            ObjectFactory<T> objectFactory) {
-        this.objectFactory = objectFactory;
-        this.size = size;
-        this.validator = validator;
+	public BoundedBlockingPool(int size, Validator<T> validator, ObjectFactory<T> objectFactory) {
+		this.objectFactory = objectFactory;
+		this.size = size;
+		this.validator = validator;
 
-        objects = new LinkedBlockingQueue<T>(size);
+		objects = new LinkedBlockingQueue<T>(size);
 
-        initializeObjects();
+		initializeObjects();
 
-        shutdownCalled = false;
-    }
+		shutdownCalled = false;
+	}
 
-    public T get(long timeOut, TimeUnit unit) {
-        if (!shutdownCalled) {
-            T t = null;
+	public T get(long timeOut, TimeUnit unit) {
+		if (!shutdownCalled) {
+			T t = null;
 
-            try {
-                t = objects.poll(timeOut, unit);
+			try {
+				t = objects.poll(timeOut, unit);
 
-                return t;
-            } catch (InterruptedException ie) {
-                Thread.currentThread().interrupt();
-            }
+				return t;
+			} catch (InterruptedException ie) {
+				Thread.currentThread().interrupt();
+			}
 
-            return t;
-        }
+			return t;
+		}
 
-        throw new IllegalStateException("Object pool is already shutdown");
-    }
+		throw new IllegalStateException("Object pool is already shutdown");
+	}
 
-    public T get() {
-        if (!shutdownCalled) {
-            T t = null;
+	public T get() {
+		if (!shutdownCalled) {
+			T t = null;
 
-            try {
-                t = objects.take();
-            } catch (InterruptedException ie) {
-                Thread.currentThread().interrupt();
-            }
+			try {
+				t = objects.take();
+			} catch (InterruptedException ie) {
+				Thread.currentThread().interrupt();
+			}
 
-            return t;
-        }
+			return t;
+		}
 
-        throw new IllegalStateException("Object pool is already shutdown");
-    }
+		throw new IllegalStateException("Object pool is already shutdown");
+	}
 
-    public void shutdown() {
-        shutdownCalled = true;
+	public void shutdown() {
+		shutdownCalled = true;
 
-        executor.shutdownNow();
+		executor.shutdownNow();
 
-        clearResources();
-    }
+		clearResources();
+	}
 
-    private void clearResources() {
-        for (T t : objects) {
-            validator.invalidate(t);
-        }
-    }
+	private void clearResources() {
+		for (T t : objects) {
+			validator.invalidate(t);
+		}
+	}
 
-    @Override
-    protected void returnToPool(T t) {
-        if (validator.isValid(t)) {
-            executor.submit(new ObjectReturner(objects, t));
-        }
-    }
+	@Override
+	protected void returnToPool(T t) {
+		if (validator.isValid(t)) {
+			executor.submit(new ObjectReturner(objects, t));
+		}
+	}
 
-    @Override
-    protected void handleInvalidReturn(T t) {
+	@Override
+	protected void handleInvalidReturn(T t) {
 
-    }
+	}
 
-    @Override
-    protected boolean isValid(T t) {
-        return validator.isValid(t);
-    }
+	@Override
+	protected boolean isValid(T t) {
+		return validator.isValid(t);
+	}
 
-    private void initializeObjects() {
-        for (int i = 0; i < size; i++) {
-            objects.add(objectFactory.createNew());
-        }
-    }
+	private void initializeObjects() {
+		for (int i = 0; i < size; i++) {
+			objects.add(objectFactory.createNew());
+		}
+	}
 
-    private class ObjectReturner implements Callable<Void> {
-        private BlockingQueue<T> queue;
-        private T t;
+	private class ObjectReturner implements Callable<Void> {
+		private BlockingQueue<T> queue;
+		private T t;
 
-        public ObjectReturner(BlockingQueue<T> queue, T t) {
-            this.queue = queue;
-            this.t = t;
-        }
+		public ObjectReturner(BlockingQueue<T> queue, T t) {
+			this.queue = queue;
+			this.t = t;
+		}
 
-        public Void call() {
-            while (true) {
-                try {
-                    queue.put(t);
-                    break;
-                } catch (InterruptedException ie) {
-                    Thread.currentThread().interrupt();
-                }
-            }
+		public Void call() {
+			while (true) {
+				try {
+					queue.put(t);
+					break;
+				} catch (InterruptedException ie) {
+					Thread.currentThread().interrupt();
+				}
+			}
 
-            return null;
-        }
-    }
+			return null;
+		}
+	}
 }
--- src/com/jobjectpool/pool/ObjectFactory.java
+++ src/com/jobjectpool/pool/ObjectFactory.java
@@ -9,10 +9,10 @@ package com.jobjectpool.pool;
  *            the type of object to create.
  */
 public interface ObjectFactory<T> {
-    /**
-     * Returns a new instance of an object of type T.
-     * 
-     * @return T an new instance of the object of type T
-     */
-    public abstract T createNew();
+	/**
+	 * Returns a new instance of an object of type T.
+	 * 
+	 * @return T an new instance of the object of type T
+	 */
+	public abstract T createNew();
 }
