27d6462faea9d483c68e56d49f274a85211d53a3
Allen Lancelot <alan910810@gmail.com>
2016-08-19 12:17:05 +0800

    Added Fork Join Example

--- src/multithreading/ForkJoinImpl.java
+++ src/multithreading/ForkJoinImpl.java
@@ -1,41 +1,75 @@
 package multithreading;
 
-import java.util.concurrent.ForkJoinPool;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+
+class Task extends RecursiveTask<Integer> {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Integer> numberList;
+	private int start;
+	private int end;
+	private int THRESHOLD = 10;
+
+	public Task(List<Integer> numberList) {
+		this.numberList = numberList;
+		start = 0;
+		end = this.numberList.size() - 1;
+	}
+
+	public Task(List<Integer> numberList, int start, int end) {
+		this.numberList = numberList;
+		this.start = start;
+		this.end = end;
+	}
+
+	@Override
+	protected Integer compute() {
+		if (end - start <= THRESHOLD) {
+			return sum();
+		} else {
+			int pivot = (end + start) / 2;
+			Task task1 = new Task(numberList, start, pivot);
+			Task task2 = new Task(numberList, pivot + 1, end);
+			task1.fork();
+			task2.fork();
+			return task1.join() + task2.join();
+		}
+
+	}
+
+	private Integer sum() {
+		Integer sum = 0;
+		for (int i = start; i <= end; i++) {
+			sum += this.numberList.get(i);
+		}
+		return sum;
+	}
+}
 
 public class ForkJoinImpl {
-	private static double[] d = { 1.2, 3.45, 5.6 };
 
-	private class ForkJoinTask extends RecursiveTask<Integer> {
+	public static void main(String[] args) throws Exception {
 
-		private static final long serialVersionUID = 1L;
-		private int first;
-		private int last;
+		ForkJoinPool pool = new ForkJoinPool();
 
-		public ForkJoinTask(int first, int last) {
-			this.first = first;
-			this.last = last;
+		Task task = new Task(getData(100));
+		Future<Integer> result = pool.submit(task);
+		System.out.println(result.get());
+	}
+
+	private static List<Integer> getData(int len) {
+
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 1; i <= len; i++) {
+			list.add(i);
 		}
-
-		protected Integer compute() {
-			int subCount;
-			if (last - first < 10) {
-				subCount = 0;
-				for (int i = first; i <= last; i++) {
-					if (d[i] < 0.5)
-						subCount++;
-				}
-			} else {
-				int mid = (first + last) >>> 1;
-				ForkJoinTask left = new ForkJoinTask(first, mid);
-				left.fork();
-				ForkJoinTask right = new ForkJoinTask(mid + 1, last);
-				right.fork();
-				subCount = left.join();
-				subCount += right.join();
-			}
-			return subCount;
-		}
+		return list;
 	}
 
 }
