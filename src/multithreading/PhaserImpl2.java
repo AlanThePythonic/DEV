6463f3d34fbdf270071f15c534d045d4ae90f2dd
Allen Lancelot <alan910810@gmail.com>
2016-10-18 16:13:07 +0800

    Added comments and re-formated

--- src/multithreading/PhaserImpl2.java
+++ src/multithreading/PhaserImpl2.java
@@ -6,20 +6,26 @@ import java.util.concurrent.TimeUnit;
 public class PhaserImpl2 {
 
 	public static void main(String[] args) {
+
 		Phaser phaser = new Phaser(1);
+
 		for (int i = 0; i < 3; i++) {
+			// char(97) = a
 			new TestPhaserThread((char) (97 + i), phaser).start();
 		}
+
 		try {
 			TimeUnit.SECONDS.sleep(3);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+
 		phaser.arrive();
 	}
 }
 
 class TestPhaserThread extends Thread {
+
 	private char c;
 	private Phaser phaser;
 
@@ -30,9 +36,14 @@ class TestPhaserThread extends Thread {
 
 	@Override
 	public void run() {
+
 		phaser.awaitAdvance(phaser.getPhase());
+
 		for (int i = 0; i < 100; i++) {
+
+			System.out.print(Thread.currentThread().getName() + " : ");
 			System.out.print(c + " ");
+
 			if (i % 10 == 9) {
 				System.out.println();
 			}
