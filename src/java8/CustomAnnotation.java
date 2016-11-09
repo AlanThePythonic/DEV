7e388a2e86d879123572b7307f74f7a9101b577e
Allen Wong <alan910810@gmail.com>
2016-11-09 18:06:30 +0800

    Added a sample call custom annotation

---
+++ src/java8/CustomAnnotation.java
@@ -0,0 +1,33 @@
+package java8;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface MyAnnotation {
+	int value() default 0;
+}
+
+// Applying annotation
+class Hello {
+	@MyAnnotation(value = 10)
+	public void sayHello() {
+		System.out.println("hello annotation");
+	}
+}
+
+// Accessing annotation
+public class CustomAnnotation {
+	public static void main(String args[]) throws Exception {
+
+		Hello h = new Hello();
+		Method m = h.getClass().getMethod("sayHello");
+
+		MyAnnotation manno = m.getAnnotation(MyAnnotation.class);
+		System.out.println("value is: " + manno.value());
+	}
+}
