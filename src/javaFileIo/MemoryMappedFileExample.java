405e335ccbbad60fc15beb618c6042ded39e7315
Allen Lancelot <alan910810@gmail.com>
2016-11-20 15:58:27 +0800

    Changed code style of MemoryMappedFileExample.java

--- src/javaFileIo/MemoryMappedFileExample.java
+++ src/javaFileIo/MemoryMappedFileExample.java
@@ -3,19 +3,19 @@ package javaFileIo;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
- 
-public class MemoryMappedFileExample 
-{
-    static int length = 0x8FFFFFF; // 128 Mb
- 
-    public static void main(String[] args) throws Exception 
-    {
-        MappedByteBuffer out = new RandomAccessFile("howtodoinjava.dat", "rw")
-                                    .getChannel().map(FileChannel.MapMode.READ_WRITE, 0, length);
-        for (int i = 0; i < length; i++)
-        {
-            out.put((byte) 'x');
-        }
-        System.out.println("Finished writing");
-    }
-} 
+
+public class MemoryMappedFileExample {
+	
+	static int length = 0x8FFFFFF; // 128 Mb
+	private static RandomAccessFile randomAccessFile;
+
+	public static void main(String[] args) throws Exception {
+
+		randomAccessFile = new RandomAccessFile("howtodoinjava.dat", "rw");
+		MappedByteBuffer out = randomAccessFile.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, length);
+		for (int i = 0; i < length; i++) {
+			out.put((byte) 'x');
+		}
+		System.out.println("Finished writing");
+	}
+}
